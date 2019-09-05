333263a96b39042e1629586ad88f8473e61fd618
Wanght <wanghaotian92@163.com>
2019-09-05 16:31:37 +0800

    初始添加项目

---
+++ HighChartTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HighChartTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult HighChart()
+        {
+            ViewBag.Message = "图表测试页面.";
+            return View();
+        }
+    }
+}
---
+++ HighChartTest/wwwroot/Views/Home/HighChart_Test.js
@@ -0,0 +1,190 @@
+﻿var chart = Highcharts.chart('container', {
+		title: {
+				text: '2010 ~ 2016 年太阳能行业就业人员发展情况'
+		},
+		subtitle: {
+				text: '数据来源：thesolarfoundation.com'
+		},
+		yAxis: {
+				title: {
+						text: '就业人数'
+				}
+		},
+		legend: {
+				layout: 'vertical',
+				align: 'right',
+				verticalAlign: 'middle'
+		},
+		plotOptions: {
+				series: {
+						label: {
+								connectorAllowed: false
+						},
+						pointStart: 2010
+				}
+		},
+		series: [{
+				name: '安装，实施人员',
+				data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+		}, {
+				name: '工人',
+				data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+		}, {
+				name: '销售',
+				data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+		}, {
+				name: '项目开发',
+				data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+		}, {
+				name: '其他',
+				data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+		}],
+		responsive: {
+				rules: [{
+						condition: {
+								maxWidth: 500
+						},
+						chartOptions: {
+								legend: {
+										layout: 'horizontal',
+										align: 'center',
+										verticalAlign: 'bottom'
+								}
+						}
+				}]
+		}
+});
+
+var chart = Highcharts.chart('container1', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: '两地月平均温度'
+    },
+    subtitle: {
+        text: '数据来源: WorldClimate.com'
+    },
+    xAxis: {
+        categories: ['一月', '二月', '三月', '四月', '五月', '六月',
+                     '七月', '八月', '九月', '十月', '十一月', '十二月']
+    },
+    yAxis: {
+        title: {
+            text: '温度'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [{
+        name: '东京',
+        marker: {
+            symbol: 'square'
+        },
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+            y: 26.5,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/demo/gfx/sun.png)'
+            }
+        }, 23.3, 18.3, 13.9, 9.6]
+    }, {
+        name: '伦敦',
+        marker: {
+            symbol: 'diamond'
+        },
+        data: [{
+            y: 3.9,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/demo/gfx/snow.png)'
+            }
+        }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+    }]
+});
+
+
+var chart = Highcharts.chart('container3', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: '两地月平均温度'
+    },
+    subtitle: {
+        text: '数据来源: WorldClimate.com'
+    },
+    xAxis: {
+        categories: ['一月', '二月', '三月', '四月', '五月', '六月',
+                     '七月', '八月', '九月', '十月', '十一月', '十二月']
+    },
+    yAxis: {
+        title: {
+            text: '温度'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [{
+        name: '东京',
+        marker: {
+            symbol: 'square'
+        },
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+            y: 26.5,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/demo/gfx/sun.png)'
+            }
+        }, 23.3, 18.3, 13.9, 9.6]
+    }, {
+        name: '伦敦',
+        marker: {
+            symbol: 'diamond'
+        },
+        data: [{
+            y: 3.9,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/demo/gfx/snow.png)'
+            }
+        }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+    }]
+});
+//$('.carousel').carousel()
+$('.carousel').carousel('dispose')
+
+//$(".tabBar").slide({
+//    mainCell: ".conWrap",
+//    effect: "left",
+//    trigger: "click",
+//    pnLoop: false
+//});
